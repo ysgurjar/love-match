c490e8da01132b0c5a1a5eacfc6bf11bbbf3acba
Yash Gurjar <ys.gurjar@gmail.com>
2023-04-20 09:42:11 +0000

    Display addition question to the user

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,21 +9,31 @@ document.addEventListener("DOMContentLoaded",function() {
                 alert("You clicked submit!");
             } else {
                 let gameType=this.getAttribute("data-type");
-                alert(`You clicked ${gameType} !`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
+
 })
 
 /**
  * The main "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
 
     //Creates two random numbers between 1 and 25
     let num1=Math.floor(Math.random()*25)+1;
     let num2=Math.floor(Math.random()*25)+1;
+
+    if (gameType=="addition") {
+        displayAdditionalQuestion(num1, num2);
+    } else {
+        alert (`Unknown game type: ${gameType}`);
+        throw ` Unknown game type: ${gameType}. Aborting` ;
+    }
 }
 
 function checkAnswer() {
@@ -42,8 +52,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionalQuestion() {
-
+function displayAdditionalQuestion(operand1,operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
