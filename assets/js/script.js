d52a81c4d48a5b36f326871bead5018b4a74eb58
Yash Gurjar <ys.gurjar@gmail.com>
2023-04-25 04:08:09 +0000

    fix: make sure the division results in integer

--- assets/js/script.js
+++ assets/js/script.js
@@ -114,7 +114,7 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1*operand2 : operand2;
     document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
 }
