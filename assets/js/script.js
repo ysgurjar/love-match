faa70380897f7481cdcb8a0cd42a0c44d672283f
Yash Gurjar <ys.gurjar@gmail.com>
2023-04-25 01:27:39 +0000

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,8 +44,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        increamentScore()
     } else {
         alert(`Awwww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]} !`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -68,13 +70,20 @@ function calculateCorrectAnswer() {
 
 
 }
-
+/**
+ * Get the current score from DOM and increment it by 1
+ */
 function increamentScore() {
-
+    let oldScore= parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Get the current tally of incorrect answers from DOM and increment it by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore= parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
